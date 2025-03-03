c3a5f03b174af2435ed105eaf65921b326051a75
Tezzy12 <lrt663@icloud.com>
2025-03-03 14:50:45 -0600

    Add files via upload

---
+++ gamble.py
@@ -0,0 +1,73 @@
+import random as rd
+global money
+money = 20 
+
+
+#bulk code of input
+def run():
+    global money
+    pick = input("would you like to gamble to 1/3,1/2 or 2/1 ")
+    if pick == "1/2":
+        gam = int(input("how much would you like to gamble"))
+        
+        if gam > money:
+            print("cant gamble that much!")
+            run()
+        else:
+            money = money - gam
+            cpu = rd.randint(1,3)
+            if cpu == 1 or cpu == 3:
+                money = (gam *1.5)+money
+                print("congrate you won! you now have")
+            else:
+                money = money - gam
+                print("you lost! you now have")
+    elif pick == "1/3":
+        gam = int(input("how much would you like to gamble"))
+        
+        if gam > money:
+            print("cant gamble that much!")
+            run()
+        else:
+            money = money - gam
+            cpu = rd.randint(1,4)
+            if cpu == 2 :
+                print("you lost! you now have")
+                money = money - gam
+            else:
+                money = (gam *1.3)+money
+                print("you won! you now have")
+    elif pick == "2/1":
+        gam = int(input("how much would you like to gamble"))
+        if gam > money:
+            print("cant gamble that much!")
+            run()
+        else:
+            money = money - gam
+            cpu = rd.randint(1,2)
+            if cpu == 1:
+                print("congrats you won! you now have")
+                money = (gam *2)+money
+            else:
+                print("you lost! you now have")
+                money = money - gam
+    else:
+        print("not valid try again")
+        run()
+    money = round(money,0)
+    check()
+
+def check():
+    if money >= 0:
+        print(money)
+        run()
+    else:
+        print("0")
+        print("you have lost all your money! better luck next time")
+
+
+
+#start text
+
+print("hello! welcome to gambling! you start with 20$ can you profit your way to 1000?")
+run()
